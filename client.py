a3b1b67de5171c778e8ad5419540d68621f558e2
virusundetected <gaurangathavale@gmail.com>
2023-04-11 23:36:43 +0530

    Done with project

--- client.py
+++ client.py
@@ -3,7 +3,11 @@ from web3 import Web3
 import numpy as np
 import networkx as nx
 import powerlaw
+import random
+import numpy as np
+import time
 
+now = time.time()
 
 #connect to the local ethereum blockchain
 provider = Web3.HTTPProvider('http://127.0.0.1:8545')
@@ -11,8 +15,10 @@ w3 = Web3(provider)
 #check if ethereum is connected
 print(w3.is_connected())
 
+print("HIIIIIIIIIIIII")
+
 #replace the address with your contract address (!very important)
-deployed_contract_address = '0xe61266cd38b0bF096d509190A8ef034525b6CeCf'
+deployed_contract_address = '0x2a4d93542e7Ad7C772b21EB999DE568a9C6F9a1a'
 
 #path of the contract json file. edit it with your contract json file
 compiled_contract_path ="build/contracts/Payment.json"
@@ -21,6 +27,8 @@ with open(compiled_contract_path) as file:
     contract_abi = contract_json['abi']
 contract = w3.eth.contract(address = deployed_contract_address, abi = contract_abi)
 
+n = 100
+graph = [[0 for i in range(n)] for j in range(n)]
 
 def generate_power_law_distribution_graph(number_of_nodes:int):
     """ generate and return a graph following power law degree distribution 
@@ -49,16 +57,74 @@ def generate_power_law_distribution_graph(number_of_nodes:int):
                 degree_sequence[j] -= 1
                 G.add_edge(i, j)
                 G.add_edge(j, i)
+                graph[i][j] = 1
+                graph[j][i] = 1
     return G
 
-n = 100
 G = generate_power_law_distribution_graph(number_of_nodes=n)
 while not nx.is_connected(G):
     G = generate_power_law_distribution_graph(n)
 
-for i in range(1,101):
-    user_registration = contract.functions.registerUser(i,'user_{}'.format(i))
-print(user_registration)
+print(G)
+
+# sum = 0
+for i in graph:
+    print(i)
+#     for j in i:
+#         sum += j
+# print(sum)
+
+for i in range(100):
+    # user_registration = contract.functions.registerUser(i+1,'user_{}'.format(i+1)).transact({'txType':"0x3", 'from':w3.eth.accounts[0], 'gas':2409638})
+    user_registration = contract.functions.registerUser(i+1,'user_{}'.format(i+1)).transact({'txType':"0x3", 'from':w3.eth.accounts[0], 'gas':2409638})
+    # user= contract.functions.getUserId(i+1).call()
+    # print(user)
+    print(user_registration)
+
+for i in range(n):
+    for j in range(i+1, n):
+        if(graph[i][j] == 1):
+            edge_amount = int(np.round(np.random.exponential(10)+2))
+            left_edge = edge_amount//2
+            right_edge = edge_amount-left_edge
+            # print(left_edge, right_edge, edge_amount)
+            joint_acc = contract.functions.createAcc(i+1, j+1, left_edge, right_edge).transact({'txType':"0x3", 'from':w3.eth.accounts[0], 'gas':2409638})
+
+successful_txn = 0
+failed_txn = 0
+for i in range(1,1011):
+    if(i%101 == 0):
+        print('\nBatch', i/100,':')
+        print('Successful Transactions:',successful_txn)
+        print('Failed Transactions:', failed_txn)
+        print('Ratio:', successful_txn*0.01)
+        successful_txn = 0
+        failed_txn = 0
+    else:
+        acc_1 = random.randint(1,n)
+        acc_2 = random.randint(1,n)
+        while(acc_1 == acc_2):
+            acc_2 = random.randint(1,n)
+        # print(acc_1, acc_2)
+        isSuccessful = contract.functions.sendAmount(acc_1, acc_2, 1).call()
+        # print(isSuccessful)
+        if(isSuccessful):
+            contract.functions.sendAmount(acc_1, acc_2, 1).transact({'txType':"0x3", 'from':w3.eth.accounts[i%10], 'gas':24096380})
+            successful_txn += 1
+        else:
+            failed_txn += 1
+
+print(time.time()-now)
+# for i in range(10):
+#     # acc_1 = random.randint(1,100)
+#     # acc_2 = random.randint(1,100)
+#     acc_1 = i+1
+#     acc_2 = i+2
+#     # while(acc_1 == acc_2):
+#     #     acc_2 = random.randint(1,100)
+#     joint_acc = contract.functions.createAcc(acc_1, acc_2, 10, 14).transact({'txType':"0x3", 'from':w3.eth.accounts[0], 'gas':2409638})
+#     print(joint_acc)
+
 
 '''
 #Calling a contract function createAcc(uint,uint,uint)
--- contracts/Payment.sol
+++ contracts/Payment.sol
@@ -1,30 +1,142 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.4.22 <0.9.0;
+pragma solidity >=0.8.2 <0.9.0;
+//pragma solidity ^0.8.0;
 
+/*@custom:dev-run-script NatSpec
+*/
 contract Payment {
-  constructor() public {
-  }
 
-  struct User {
-      uint userId;
-      string userName;
-  }
+   mapping(uint => string) public users;
+uint[] ids;
+uint[] public path;
+   mapping (uint => mapping (uint=>uint)) public joint_account;
+   mapping(uint => bool) visited;
+  mapping(uint => bool) processed;
+    mapping(uint => uint[]) public adj_mat; 
+    uint public total_nodes;
+    struct Node {
+        uint parent; // Parent node
+        bool visited; // Flag to track visited user_nodes
+    }
+    mapping(uint => Node) public user_nodes;
 
-  mapping(address => User) users;
 
-  function registerUser(uint user_id, string memory user_name) public {
-    users[msg.sender] = User(user_id, user_name);
-  }
 
-  function createAcc(uint user_id_1, uint user_id_2, uint balance) public {
-    
-  }
 
-  function sendAmount(uint user_id_1, uint user_id_2) public payable {
-    
-  }
+   function registerUser(uint use_id, string memory user_name) public returns (uint)
+   {
+       total_nodes=total_nodes+1;
+       users[use_id]=user_name;
+       ids.push(use_id);
+       return use_id;
+   }
 
-  function closeAccount(uint user_id_1, uint user_id_2) public payable {
-    
-  }
+   function createAcc(uint user_id_1,uint user_id_2,uint balance1,uint balance2) public returns(uint)
+   {
+       joint_account[user_id_1][user_id_2]=balance1;
+       joint_account[user_id_2][user_id_1]=balance2;
+       uint[] storage existingValues1 = adj_mat[user_id_1];
+       existingValues1.push(user_id_2);
+       adj_mat[user_id_1]=existingValues1;
+       uint[] storage existingValues2 = adj_mat[user_id_2];
+       existingValues2.push(user_id_1);
+       adj_mat[user_id_2]=existingValues2;
+       return joint_account[user_id_1][user_id_2];
+   }
+
+
+    function sendAmount(uint user_id_1,uint user_id_2, uint amount) public returns(bool)
+    {
+       bool ispath;
+        delete path;
+        ispath=check_path(user_id_1,user_id_2);
+        if(!ispath) return false;
+
+        // uint[] memory path = new uint[](size);
+
+        uint curr=user_id_2;
+        while(curr!=user_id_1)
+        {
+            path.push(curr);
+            curr=getParent(curr);
+        }
+        path.push(user_id_1);
+
+        bool decision = true;
+        for(uint j=path.length - 1; j>=1; j--){
+            if(joint_account[path[j]][path[j-1]] < amount){
+                decision = false;
+                return decision;
+            }
+        }
+        for(uint j=path.length - 1; j>=1; j--){
+            joint_account[path[j]][path[j-1]] -= amount;
+            joint_account[path[j-1]][path[j]] += amount;
+        }
+        return true;
+        // return path;
+        
+    }
+
+    function check_path(uint start,uint end) public returns (bool)
+    {
+
+
+        for(uint i=0;i<total_nodes;i++)
+        {
+            user_nodes[ids[i]].parent=0;
+            user_nodes[ids[i]].visited=false;
+
+        }
+
+        uint[] memory queue = new uint[](total_nodes);
+        uint f = 0;
+        uint r = 0;
+
+        if(start==end)
+        {
+            return true;
+        }
+
+        queue[r++] = start;
+        user_nodes[start].parent = 0;
+        user_nodes[start].visited = true; 
+
+        while (f < r) {
+            uint current = queue[f++];
+
+             if (current == end) {
+                return true;
+            }
+
+             for (uint i = 0; i < adj_mat[current].length; i++) {
+                 uint neighbor = adj_mat[current][i];
+
+                if (! user_nodes[neighbor].visited) {
+                    queue[r++] = neighbor;
+                    user_nodes[neighbor].visited = true;
+                    user_nodes[neighbor].parent = current; 
+
+                }
+            }
+
+        }
+        return false;
+
+    }
+
+    function getParent(uint node) public view returns (uint) {
+        return user_nodes[node].parent;
+    }
+
+
+    function closeAccount(uint user_id_1, uint user_id_2) public
+    {
+        delete joint_account[user_id_1][user_id_2];
+        delete joint_account[user_id_2][user_id_1];
+        // bool and = true;
+
+    }
+
+    // uint a = registerUser(1, "user_1user_name");
 }
